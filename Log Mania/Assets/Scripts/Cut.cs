51a1ea0cd9b06f1cec513853c290a24a1516dee3
yaltiok <yusuf@altiok.net>
2020-09-30 22:39:37 +0300

    Log Cutting Zone

---
+++ Log Mania/Assets/Scripts/Cut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // TODO: Buraya bir şeyler yaz
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.transform.tag);
+        if (collision.transform.CompareTag("CuttingZone"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Log Mania/Assets/Scripts/LogManager.cs
+++ Log Mania/Assets/Scripts/LogManager.cs
@@ -15,6 +15,10 @@ public class LogManager : MonoBehaviour
         shapeWidth = GetComponent<MeshRenderer>().bounds.size.x;
         float offset = Random.Range(-boundary.transform.position.x, boundary.transform.position.x);
 
+        int dir = Random.Range(0,2);
+
+        transform.Rotate(dir * 180f, 0, 0);
+
         offsetVector = new Vector3(offset,0,0);
 
         transform.position += offsetVector;
